9222adbfae13ee7fab9a03c5e48e79c75d51b78a
ibrahim364 <you@example.com>
2022-07-31 23:02:37 +0100

    ignore .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { RecruitersModule } from './recruiters/recruiters.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      typePaths: ['./**/*.graphql'],
+    }),
+    UsersModule,
+    RecruitersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
